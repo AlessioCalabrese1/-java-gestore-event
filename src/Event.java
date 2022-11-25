82850773d400e37c2f8b121b672b3d0d3589a502
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-11-25 09:36:36 +0100

    Setup

---
+++ src/Event.java
@@ -0,0 +1,14 @@
+import java.sql.Date;
+
+public class Event {
+    private String title;
+    private Date date;
+    private int totalSeats;
+    private int reservedSeats = 0;
+
+    public Event(){
+
+    }
+
+    
+}

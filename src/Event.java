ce54fa8d699dbd9a36968677f8f5f3379f9fbe4e
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-11-25 10:50:39 +0100

    Event: throw exception in set Date and setTotalSeats

--- src/Event.java
+++ src/Event.java
@@ -1,12 +1,16 @@
 import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class Event {
     private String title;
-    private Date date;
+    private String date;
     private int totalSeats;
     private int reservedSeats = 0;
 
-    public Event(String title, Date date, int totalSeats){
+    public Event(String title, String date, int totalSeats) throws Exception{
         setTitle(title);
         setDate(date);
         setTotalSeats(totalSeats);
@@ -20,11 +24,26 @@ public class Event {
         this.title = title;
     }
 
-    public Date getDate() {
+    public String getDate() {
         return date;
     }
 
-    public void setDate(Date date) {
+    public void setDate(String date) throws Exception{
+        try {
+            SimpleDateFormat pattern = new SimpleDateFormat("dd/MM/yyyy");
+            Date eventD = (Date) pattern.parse(date);
+            Calendar c = Calendar.getInstance();
+            Date now = (Date) c.getTime();
+            long elapsedms = eventD.getTime() - now.getTime();
+            long diff = TimeUnit.MINUTES.convert(elapsedms, TimeUnit.MILLISECONDS);
+            System.out.println("Diff: " + diff);
+            if (diff < 0) {
+                throw new Exception("La data inserita deve essere maggiore di quella corrente!");
+            }
+        } catch (Exception e) {
+            System.err.println(e);
+        }
+
         this.date = date;
     }
 
@@ -36,9 +55,16 @@ public class Event {
         return reservedSeats;
     }
 
-    private void setTotalSeats(int totalSeats) {
+    private void setTotalSeats(int totalSeats) throws Exception{
+        if (totalSeats < 0) {
+            throw new Exception("Il numero di posti totali deve essere positivo!");
+        }
         this.totalSeats = totalSeats;
     }
 
-    
+    @Override
+    public String toString() {
+        return "L'evento aggiunto è: " + "\nTitle: " + title + "\nDate: " + date + "\nPosti totali: "
+         + totalSeats + "\nPosti riservati: " + reservedSeats;
+    }
 }

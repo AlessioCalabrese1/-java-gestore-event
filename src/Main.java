20885c72c4a294bbe69de884302a45a00979a519
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-11-25 14:10:05 +0100

    Milestone 3

--- src/Main.java
+++ src/Main.java
@@ -1,50 +1,71 @@
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDate eventDate = LocalDate.parse("2023-06-13");
         Scanner sn = new Scanner(System.in);
-        Event event = null;
+        ProgrammEvents progEvents = new ProgrammEvents("Eventi a Milano");
         try {
-            event = new Event("Nuovo Nuovo", eventDate, 15);
-            System.out.println(event);
+            LocalDate eventDate1 = LocalDate.parse("2027-06-13");
+            Event event1 = new Event("Nuovo Nuovo 1", eventDate1, 150);
+
+            LocalDate eventDate2 = LocalDate.parse("2024-09-25");
+            Event event2 = new Event("Nuovo Nuovo 2", eventDate2, 300);
+
+            LocalDate eventDate3 = LocalDate.parse("2025-09-25");
+            Event event3 = new Event("Nuovo Nuovo 3", eventDate3, 200);
+            
+            progEvents.addEvent(event1);
+            progEvents.addEvent(event2);
+            progEvents.addEvent(event3);
+
+            System.out.println(progEvents);
             System.out.println("-------------------------------");
+
+            System.out.println(progEvents.howManyEvents());
+            System.out.println("-------------------------------");
+
+            List<Event> eventsOnDate = progEvents.getEventsOnSelectedDate(eventDate3);
+            System.out.println("Gli eventi alla data " + eventDate3 + " sono:\n" + eventsOnDate);
+            System.out.println("-------------------------------");
+
+            System.out.println(progEvents.getEventsSortedByDate());
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
 
-        boolean sentinel = true;
-        while (sentinel) {
-            System.out.println("Si vuole fare delle prenotazioni? (y/n)");
-            String conf = sn.next();
-            if (conf.equals("y")) {
-                System.out.println("-------------------------------");
-                System.out.println("Quante prenotazioni si vuole effettuare?");
-                int nReservations = sn.nextInt();
-                event.book(nReservations);
-                System.out.println(event);
+        // boolean sentinel = true;
+        // while (sentinel) {
+        //     System.out.println("Si vuole fare delle prenotazioni? (y/n)");
+        //     String conf = sn.next();
+        //     if (conf.equals("y")) {
+        //         System.out.println("-------------------------------");
+        //         System.out.println("Quante prenotazioni si vuole effettuare?");
+        //         int nReservations = sn.nextInt();
+        //         event.book(nReservations);
+        //         System.out.println(event);
 
-                System.out.println("-------------------------------");
-                System.out.println("Si vogliono cancellare delle prenotazioni? (y/n)");
-                conf = sn.next();
-                if (conf.equals("y")) {
-                    System.out.println("-------------------------------");
-                    System.out.println("Quante prenotazioni si desidera cancellare?");
-                    int nCancel = sn.nextInt();
-                    event.cancel(nCancel);
-                    System.out.println(event);
-                    System.out.println("-------------------------------");
-                }
-            }else if(conf.equals("n")){
-                System.out.println("-------------------------------");
-                System.out.println(event);
-                System.out.println("Arrivederci e buona giornata!");
-                sentinel = false;
-            }else{
-                System.out.println("Il valore inserito non è corretto! Riprovare!");
-                System.out.println("-------------------------------");
-            }
-        }
+        //         System.out.println("-------------------------------");
+        //         System.out.println("Si vogliono cancellare delle prenotazioni? (y/n)");
+        //         conf = sn.next();
+        //         if (conf.equals("y")) {
+        //             System.out.println("-------------------------------");
+        //             System.out.println("Quante prenotazioni si desidera cancellare?");
+        //             int nCancel = sn.nextInt();
+        //             event.cancel(nCancel);
+        //             System.out.println(event);
+        //             System.out.println("-------------------------------");
+        //         }
+        //     }else if(conf.equals("n")){
+        //         System.out.println("-------------------------------");
+        //         System.out.println(event);
+        //         System.out.println("Arrivederci e buona giornata!");
+        //         sentinel = false;
+        //     }else{
+        //         System.out.println("Il valore inserito non è corretto! Riprovare!");
+        //         System.out.println("-------------------------------");
+        //     }
+        // }
     }
 }
--- src/ProgrammEvents.java
+++ src/ProgrammEvents.java
@@ -1,5 +1,11 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class ProgrammEvents {
     
@@ -11,8 +17,42 @@ public class ProgrammEvents {
         events = new ArrayList<>();
     }
 
-    public void setTitle(String title) {
+    private void setTitle(String title) {
         this.title = title;
     }
+
+    public void addEvent(Event event){
+        events.add(event);
+    }
     
+    public String howManyEvents(){
+        return "Nel programma ci sono: " + events.size() + " eventi";
+    }
+
+    public void cancelAllEvents(){
+        events.clear();
+        System.out.println("La lista di eventi è stata svuotata!");
+    }
+
+    public List<Event> getEventsOnSelectedDate(LocalDate date){
+        List<Event> eventsOnSelectedDate = new ArrayList<>();
+        for (Event event : events) {
+            if (event.getDate().isEqual(date)) {
+                eventsOnSelectedDate.add(event);
+            }
+        }
+        return eventsOnSelectedDate;
+    }
+
+    public String getEventsSortedByDate(){
+        //comparator svolto con Lamba Exspression
+        Comparator<Event> comparatorAsc = (event1, event2) -> event1.getDate().compareTo(event2.getDate());
+        Collections.sort(events, comparatorAsc);
+        return "Gli eventi ordinati per data sono: " + events;
+    }
+
+    @Override
+    public String toString() {
+        return "Gli eventi in programma sono: \n" + events;
+    }
 }
